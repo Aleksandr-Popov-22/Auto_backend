e18a89453e455a6db505d62afc7e0c1424b30a8d
Aleksandr-Popov-22 <43619329+Aleksandr-Popov-22@users.noreply.github.com>
2025-05-06 23:52:44 +0300

    Update urls.py

--- Auto_backend/Auto_backend/urls.py
+++ Auto_backend/Auto_backend/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for Auto_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from Auto import views
 from django.urls import include, path
@@ -24,8 +8,8 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('', include(router.urls)),
     path(r'marks/', views.MarkList.as_view(), name='marks-list'),
-    path(r'marks/<string:id>/', views.ModelList.as_view(), name='models-list'),
-    path(r'marks/<string:id>/<string:id>/', views.ModelDetail.as_view(), name='model-detail'),
+    path(r'marks/<str:id>/', views.ModelList.as_view(), name='models-list'),
+    #spath(r'marks/<string:id>/<string:id>/', views.ModelDetail.as_view(), name='model-detail'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
