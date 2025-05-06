d8e9d266f8e68daff3e61007310f2054ddc0397b
Aleksandr-Popov-22 <43619329+Aleksandr-Popov-22@users.noreply.github.com>
2025-05-06 23:51:40 +0300

    Update serializers.py

--- Auto_backend/Auto/serializers.py
+++ Auto_backend/Auto/serializers.py
@@ -1,4 +1,4 @@
-from Auto.models import Mark, Configuration
+from Auto.models import Mark, Configuration, Model
 #from Auto.models import AuthUser
 from rest_framework import serializers
 
@@ -9,6 +9,14 @@ class MarkSerializer(serializers.ModelSerializer):
         model = Mark
         # Поля, которые мы сериализуем
         fields = ["id", "name", "country"]
+
+
+class ModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        # Модель, которую мы сериализуем
+        model = Model
+        # Поля, которые мы сериализуем
+        fields = ["id", "name"]
 
 
 class ConfigurationSerializer(serializers.ModelSerializer):
