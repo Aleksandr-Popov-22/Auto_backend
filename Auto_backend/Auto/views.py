1d825883cd679f138410433701bf2de91d547886
Aleksandr-Popov-22 <43619329+Aleksandr-Popov-22@users.noreply.github.com>
2025-05-06 23:52:09 +0300

    Update views.py

--- Auto_backend/Auto/views.py
+++ Auto_backend/Auto/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from Auto.serializers import MarkSerializer
+from Auto.serializers import MarkSerializer, ModelSerializer
 from Auto.models import Mark, Model
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
@@ -12,7 +12,6 @@ class MarkList(APIView):
     model_class = Mark
     serializer_class = MarkSerializer
 
-    # Возвращает список акций
     def get(self, request, format=None):
         
         marks = self.model_class.objects.all()
@@ -28,19 +27,15 @@ class MarkList(APIView):
     
 class ModelList(APIView):
     model_class = Model
-    serializer_class = MarkSerializer
+    serializer_class = ModelSerializer
 
-    # Возвращает список акций
-    def get(self, request, format=None):
+    def get(self, request, id, format=None):
+
+        models = self.model_class.objects.all().filter(mark=id)
+
         
-        marks = self.model_class.objects.all()
-        search = self.request.query_params.get('search')
-        if search is not None:
-            marks = marks.filter(name=search)
-        
-        serializer = self.serializer_class(marks, many=True)
+        serializer = self.serializer_class(models, many=True)
 
     
 
         return Response(serializer.data)
-    
