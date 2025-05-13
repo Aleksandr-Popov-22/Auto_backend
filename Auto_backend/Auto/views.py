51373b082001b811abae16461c89f6b7d2ede9d6
Aleksandr-Popov-22 <43619329+Aleksandr-Popov-22@users.noreply.github.com>
2025-05-13 20:00:20 +0300

    Update views.py

--- Auto_backend/Auto/views.py
+++ Auto_backend/Auto/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from Auto.serializers import MarkSerializer, ModelSerializer
-from Auto.models import Mark, Model
+from Auto.serializers import MarkSerializer, ModelSerializer, GenerationSerializer, GenerationCutSerializer, GenerationCharacteristicSerializer
+from Auto.models import Mark, Model, Generation, Configuration, Modification, Options, Specifications
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 
@@ -13,15 +13,13 @@ class MarkList(APIView):
     serializer_class = MarkSerializer
 
     def get(self, request, format=None):
-        
-        marks = self.model_class.objects.all()
-        search = self.request.query_params.get('search')
-        if search is not None:
-            marks = marks.filter(name=search)
-        
-        serializer = self.serializer_class(marks, many=True)
 
-    
+        marks = Mark.objects.all()
+
+        if request.GET.get('search'):
+            marks = Mark.objects.filter(name__icontains=request.GET.get('search'))       
+        
+        serializer = self.serializer_class(marks, many=True)    
 
         return Response(serializer.data)
     
@@ -37,5 +35,36 @@ class ModelList(APIView):
         serializer = self.serializer_class(models, many=True)
 
     
+
+        return Response(serializer.data)
+    
+
+class ModelDetail(APIView):
+    model_class = Generation
+    serializer_class = GenerationSerializer
+
+    def get(self, request, id, id_model, format=None):
+
+
+        generations = Generation.objects.all().prefetch_related('configurations').filter(model=id_model)
+
+
+        serializer = GenerationCutSerializer(generations, many=True)
+
+        return Response(serializer.data)
+    
+
+class ModelDetailCharacteristic(APIView):
+
+    def get(self, request, id, id_model, format=None):
+
+        '''generations = Generation.objects.all().prefetch_related(
+            'configurations__modifications__options',
+            'configurations__modifications__specifications'
+        ).filter(model=id_model)'''
+        generations = Generation.objects.all().filter(model=id_model)
+
+
+        serializer = GenerationCharacteristicSerializer(generations, many=True)
 
         return Response(serializer.data)
