4eb54d9551273103fd0a7bb091e8c07d40de7c1f
Aleksandr-Popov-22 <43619329+Aleksandr-Popov-22@users.noreply.github.com>
2025-05-13 19:59:45 +0300

    Update serializers.py

--- Auto_backend/Auto/serializers.py
+++ Auto_backend/Auto/serializers.py
@@ -1,4 +1,4 @@
-from Auto.models import Mark, Configuration, Model
+from Auto.models import Mark, Configuration, Model, Generation, Modification, Options, Specifications
 #from Auto.models import AuthUser
 from rest_framework import serializers
 
@@ -19,9 +19,113 @@ class ModelSerializer(serializers.ModelSerializer):
         fields = ["id", "name"]
 
 
+class OptionsSerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        # Модель, которую мы сериализуем
+        model = Options
+        # Поля, которые мы сериализуем
+        fields = '__all__'
+
+
+
+class SpecificationsSerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        # Модель, которую мы сериализуем
+        model = Specifications
+        # Поля, которые мы сериализуем
+        fields = '__all__'
+
+
+class ModificationSerializer(serializers.ModelSerializer):
+
+    options = OptionsSerializer(many=True, read_only=True)
+    specifications = SpecificationsSerializer(many=True, read_only=True)
+
+
+    class Meta:
+        # Модель, которую мы сериализуем
+        model = Modification
+        # Поля, которые мы сериализуем
+        fields = ['complectation_id', 'group_name', 'options', 'specifications']
+
 class ConfigurationSerializer(serializers.ModelSerializer):
+
+    modifications = ModificationSerializer(many=True, read_only=True)
+
+
     class Meta:
         # Модель, которую мы сериализуем
         model = Configuration
         # Поля, которые мы сериализуем
-        fields = []
+        fields = ['id', 'doors_count', 'body_type', 'configuration_name', 'modifications']
+
+
+
+class ConfigurationCutSerializer(serializers.ModelSerializer):
+
+
+
+    class Meta:
+        # Модель, которую мы сериализуем
+        model = Configuration
+        # Поля, которые мы сериализуем
+        fields = ['id', 'doors_count', 'body_type', 'configuration_name']
+
+
+
+
+
+class GenerationSerializer(serializers.ModelSerializer):
+
+    configurations = ConfigurationSerializer(many=True, read_only=True)
+    
+
+
+
+    class Meta:
+        # Модель, которую мы сериализуем
+        model = Generation
+        # Поля, которые мы сериализуем
+        fields = ['id', 'name', 'year_start', 'year_stop', 'is_restyle', 'model', 'configurations']
+
+
+class GenerationCutSerializer(serializers.ModelSerializer):
+
+    configurations = ConfigurationCutSerializer(many=True, read_only=True)
+    
+
+
+
+    class Meta:
+        # Модель, которую мы сериализуем
+        model = Generation
+        # Поля, которые мы сериализуем
+        fields = ['id', 'name', 'year_start', 'year_stop', 'is_restyle', 'model', 'configurations']
+
+
+
+
+
+
+
+
+
+
+
+class GenerationCharacteristicSerializer(serializers.ModelSerializer):
+
+    configurations = ConfigurationSerializer(many=True, read_only=True)
+
+
+    class Meta:
+        # Модель, которую мы сериализуем
+        model = Generation
+        # Поля, которые мы сериализуем
+        fields = ['id', 'name', 'year_start', 'year_stop', 'is_restyle', 'model', 'configurations']
+
+
+
